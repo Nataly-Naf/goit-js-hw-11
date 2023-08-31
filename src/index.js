feb161161b1b7301a966a46b188366ca5a810998
Nataly-Naf <nat.spivak84@gmail.com>
2023-08-31 18:27:40 +0100

    lightbox

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import "simplelightbox/dist/simple-lightbox.min.css";
 import { createMarkup, getImages } from "./js/api";
 import { refs } from "./js/refs";
 
-     const lightbox = new SimpleLightbox('.gallery__link', { 
-            showImageNumberLabel: false,
+     const lightbox = new SimpleLightbox('.all-items-gallery a', { 
+            showImageNumberLabel: true,
             overlay: false,
             captionDelay: 250,
             animationSlide: true,
@@ -13,34 +13,27 @@ import { refs } from "./js/refs";
         });
 refs.form.addEventListener('submit', onSubmitBtnClick)
 
-
 function onSubmitBtnClick(event) {
     event.preventDefault();
-    refs.gallery.innerHTML=''
-     const searchItem = event.currentTarget[0].value;
-    
-    let page = 1
-    
+    refs.gallery.innerHTML = '';
+    const searchItem = event.currentTarget[0].value;
+    let page = 1;
     getImages(searchItem, page)
-        .then((resp) => refs.gallery.insertAdjacentHTML('beforeend', createMarkup(resp.hits)
-        ));
-    console.log (lightbox.refresh())
-       
-
-     
-   
-    refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick)
+        .then((resp) => {
+            refs.gallery.insertAdjacentHTML('beforeend', createMarkup(resp.hits));
+            lightbox.refresh(); // Виклик refresh після додавання зображень
+        });
+    refs.loadMoreBtn.addEventListener('click', onLoadMoreBtnClick);
     function onLoadMoreBtnClick() {
-    page += 1
-    console.log('click')
+        page += 1;
         getImages(searchItem, page)
-            .then((resp) => refs.gallery.insertAdjacentHTML('beforeend', createMarkup(resp.hits))
-        )
-        lightbox.refresh()
-        
-    
+            .then((resp) => {
+                refs.gallery.insertAdjacentHTML('beforeend', createMarkup(resp.hits));
+                lightbox.refresh(); // Виклик refresh після додавання зображень
+            });
+    }
 }
-   
-}
+
+
 
 
